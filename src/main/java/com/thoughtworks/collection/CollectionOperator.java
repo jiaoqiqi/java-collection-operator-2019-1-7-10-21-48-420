6e0f6a41f622f84815a14b4a093ca02a26c46c10
jiaoqiqi <jiaoqiqi977@163.com>
2019-03-29 21:54:54 +0800

    complete collection operator

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,30 +2,78 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        if (left<right){
+            for (int i = left; i <= right; i++) {
+                result.add(i);
+            }
+        }else{
+            for (int i = left; i >= right; i--) {
+                result.add(i);
+            }
+        }
+        return  result;
     }
-
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        if (left<right){
+            for (int i = left; i <= right; i++) {
+                if (i%2==0){
+                    result.add(i);
+                }
+            }
+        }else{
+            for (int i = left; i >= right; i--) {
+                if (i%2==0){
+                    result.add(i);
+                }
+            }
+        }
+        return  result;
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for (int item : array) {
+            if (item%2 == 0 ){
+                result.add(item);
+            }
+        }
+        return  result;
     }
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+        return array[array.length-1];
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for (int itemFirst : firstArray) {
+            for (int itemSecond : secondArray) {
+                if (itemSecond==(itemFirst)){
+                    result.add(itemFirst);
+                }
+
+            }
+        }
+        return  result;
     }
 
     public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
-        throw new NotImplementedException();
-    }
-}
+        List<Integer> result = new ArrayList<>();
+        for (Integer itemFirst : firstArray) {
+            result.add(itemFirst);
+        }
+        for (Integer itemSecond : secondArray) {
+            if (!result.contains(itemSecond)){
+                result.add(itemSecond);
+            }
+        }
+
+        return  result;
+    }}
